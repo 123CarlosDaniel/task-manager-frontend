d544b536c0bc8190ad8d4a5146e547eafaa79e6d
cdcm <carloss_jt@hotmail.com>
2024-12-26 15:14:01 -0500

    chore: add create task button

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,8 +1,15 @@
+import TaskDialog from "@/components/task/TaskDialog"
 import TasksList from "@/components/task/TasksList"
+import { Button } from "@/components/ui/button"
 
 function TasksPage() {
   return (
     <div className="w-full">
+      <div className="sticky top-0 flex justify-end w-full pt-4 pb-2 bg-white bg-opacity-10 pe-7 backdrop-blur-sm">
+        <TaskDialog mode="create">
+          <Button type="button">Create task</Button>
+        </TaskDialog>
+      </div>
       <TasksList />
     </div>
   )
