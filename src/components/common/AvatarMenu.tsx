1f104289f7158d47785cdc5224c0f8ed305dcd61
cdcm <carloss_jt@hotmail.com>
2024-12-26 14:33:25 -0500

    chore: update avatar menu

--- src/components/common/AvatarMenu.tsx
+++ src/components/common/AvatarMenu.tsx
@@ -9,6 +9,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { logOut } from "@/services/authService"
+import { Link } from "react-router"
+import { IoMdInformationCircleOutline } from "react-icons/io"
+import { IoLogOutOutline } from "react-icons/io5"
+import { FaTasks } from "react-icons/fa";
 
 const AvatarMenu = ({ avatarUrl }: { avatarUrl: string }) => {
   return (
@@ -21,10 +25,25 @@ const AvatarMenu = ({ avatarUrl }: { avatarUrl: string }) => {
       <DropdownMenuContent>
         <DropdownMenuLabel>Actions</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem onClick={logOut}>Logout</DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to={"/"} className="flex items-center w-full gap-x-2">
+            <IoMdInformationCircleOutline />
+            Information
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to={"/tasks"} className="flex items-center w-full gap-x-2">
+            <FaTasks />
+            Tasks
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={logOut}
+          className="flex items-center cursor-pointer gap-x-2"
+        >
+          <IoLogOutOutline />
+          Logout
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
